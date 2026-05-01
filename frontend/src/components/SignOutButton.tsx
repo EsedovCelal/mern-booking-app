510de8184ecb0f77382ef98568c278b713dd1b51
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2026-05-01 19:23:40 +0400

    fix: add backend and frontend as regular folders

---
+++ frontend/src/components/SignOutButton.tsx
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+
+const SignOutButton = () => {
+  const { showToast } = useAppContext();
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: apiClient.signOut,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["validateToken"] });
+      showToast({ message: "Signed Out", type: "SUCCESS" });
+    },
+    onError: (error) => showToast({ message: error.message, type: "ERROR" }),
+  });
+  const handleClick = () => {
+    mutation.mutate();
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className="text-blue-600 px-3 font-bold bg-white hover:bg-gray-100"
+    >
+      Sign Out
+    </button>
+  );
+};
+
+export default SignOutButton;
