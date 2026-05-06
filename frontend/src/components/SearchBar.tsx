b9752acf6e0219b4b45ede37ff67dd50b2f6b0d2
Jalal <107685956+EsedovCelal@users.noreply.github.com>
2026-05-06 16:44:00 +0400

    add search function in backend

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { useSearchContext } from "../contexts/SearchContext";
+
+const SearchBar = () => {
+  const search = useSearchContext();
+
+  const [destination, setDestination] = useState<string>(search.destination);
+  const [checkIn, setCheckIn] = useState<Date>(search.checkIn);
+  const [checkOut, setCheckOut] = useState<Date>(search.checkOut);
+  const [adultCount, setAdultCount] = useState<number>(search.adultCount);
+  const [childCount, setChildCount] = useState<number>(search.childCount);
+};
